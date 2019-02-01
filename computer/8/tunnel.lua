190d018c76cb45b9f5dacf7fa800b3438d154a44
artem <tema.antonenko@ukr.net>
2019-02-01 23:08:52 +0200

    added metro station

---
+++ computer/8/tunnel.lua
@@ -0,0 +1,22 @@
+turtle.refuel(1)
+while true do
+turtle.dig()
+turtle.forward()
+turtle.digDown()
+turtle.digUp()
+turtle.up()
+turtle.digUp()
+turtle.up()
+turtle.digUp()
+turtle.Up()
+turtle.digUp()
+turtle.down()
+turtle.down()
+turtle.down()
+turtle.down()
+while turtle.detectUp() do 
+turtle.digUp()
+os.sleep(0.5)
+end
+end
+
